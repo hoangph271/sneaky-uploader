96bf4fa0dd0485edc37d8abb55d93c7c1ca03db5
Huy Hoang Phan <hoangph271@gmail.com>
2021-06-16 19:45:38 +0700

    [Fix] ESLint fixes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, shell } from 'electron'
+import { app, BrowserWindow } from 'electron'
 import { startServer } from './server'
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string
